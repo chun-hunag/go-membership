17cbc090106d3521c4edbd4327a3cf237015f2a5
chun-huang <hgfrttlk@gmail.com>
2022-05-22 23:40:24 +0800

    Update users migration

--- database/migrations/20220522073501_create_users_table.up.sql
+++ database/migrations/20220522073501_create_users_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     id integer NOT null PRIMARY KEY,
     name varchar(10) not null,
     email varchar(40) unique not null,
-    remember_token varchar(100) not null,
+    remember_token varchar(100),
     created_at timestamp,
     updated_at timestamp
 );
